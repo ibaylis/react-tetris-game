f250bccfd688f8595401c8ad25af3870c63839ac
ibaylis <ibay0918@gmail.com>
2019-09-22 21:23:33 -0400

    My first commit of tetris game

---
+++ src/components/Cell.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
+
+const Cell = ({ type }) => (
+        <StyledCell 
+            type={'L'} 
+            color={TETROMINOS['L'].color}
+        />
+    )
+
+export default Cell;
